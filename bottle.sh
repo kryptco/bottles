b39b66717499c208523e80d09d92a672ab202028
Kevin King <kevin@krypt.co>
2018-01-07 19:17:51 -0500

    use FETCH_HEAD to checkout origin/devel

--- bottle.sh
+++ bottle.sh
@@ -25,7 +25,7 @@ rm -rf ~/Library/Caches/Homebrew/kr--git
 brew tap kryptco/tap
 brew install --verbose --build-bottle kryptco/tap/kr || true
 
-cd /usr/local/Homebrew/Library/Taps/kryptco/homebrew-tap && git fetch && git checkout -B devel && git reset --hard origin/devel && cd -
+cd /usr/local/Homebrew/Library/Taps/kryptco/homebrew-tap && git fetch origin devel && git checkout FETCH_HEAD && git checkout -B devel && cd -
 
 bottle_name=kr-$version.$osx_name.bottle.${rebuild}tar.gz
 brew bottle kryptco/tap/kr
