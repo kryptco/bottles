31c49b304be4f19abba7426c8985dfb6f6731247
Kevin King <kevin@krypt.co>
2018-01-07 19:32:51 -0500

    remove bottle.sh args

--- bottle.sh
+++ bottle.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
 set -e
-version=$1
-osx_name=$2
 
-rebuild=$3
-if [[ ! -z "$rebuild" ]]; then
-	rebuild="${rebuild}."
-fi
-
-usage="usage: bottle.sh <VERSION> <OS NAME>"
-if [[ -z $version ]]; then
-	echo $usage 
-	exit 1
-fi
-if [[ -z $osx_name ]]; then
-	echo $usage 
-	exit 1
-fi
 rm -rf tmp || true
 mkdir tmp 
 cd tmp
+
+cd /usr/local/Homebrew/Library/Taps/kryptco/homebrew-tap && git fetch origin devel && git checkout FETCH_HEAD && git checkout -B devel && cd -
+
 brew uninstall kr || true
 rm -rf ~/Library/Caches/Homebrew/kr--git
 brew tap kryptco/tap
 brew install --verbose --build-bottle kryptco/tap/kr || true
 
-cd /usr/local/Homebrew/Library/Taps/kryptco/homebrew-tap && git fetch origin devel && git checkout FETCH_HEAD && git checkout -B devel && cd -
-
-bottle_name=kr-$version.$osx_name.bottle.${rebuild}tar.gz
 brew bottle kryptco/tap/kr
-mv kr-* $bottle_name
+bottle_name=`ls`
 tar xvf $bottle_name
 rm -f $bottle_name
 
@@ -47,4 +31,4 @@ hash=`openssl dgst -sha256 ../$bottle_name | awk '{print $2}'`
 echo $hash
 cd ..
 git add $bottle_name
-git commit -m "$osx_name $version.$rebuild SHA256: $hash"
+git commit -m "$bottle_name SHA256: $hash"
